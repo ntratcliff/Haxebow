37db318676e4b5ad108b2b6d784ae511f6ead327
Noah Ratcliff <me@nratcliff.com>
2016-01-14 17:54:41 -0500

    basic statement interpreting

--- Rainbow.hx
+++ Rainbow.hx
@@ -1,6 +1,7 @@
 class Rainbow {
 	static public function main() { 
 		var parser = new Parser(Sys.args()); //create a new instance of the parser and pass command line arguments
-		
+		var interpreter = new Interpreter(parser.statements);
+		interpreter.begin();
 	}
 }
